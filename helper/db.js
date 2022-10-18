78e0a5df547a2e767f9135035382e5e27a194924
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-10-18 23:21:02 +0300

    make it better

--- helper/db.js
+++ helper/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    mongoose.connect('mongodb+srv://movie_user:abcd1234@cluster0.g3tjmb3.mongodb.net/movie-api');
+    mongoose.connect('mongodb+srv://movie_user:abcd1234@cluster0.g3tjmb3.mongodb.net/?retryWrites=true&w=majority');
     mongoose.connection.on('open', () => {
         console.log('MongoDB: Connected');
     });
